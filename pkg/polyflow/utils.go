5c5c099a1dccd47311d37a58a8b6f3ea1a4b7f65
Arnaud Aliès <arnaudalies.py@gmail.com>
2026-05-09 17:34:05 +0200

    Move things around

--- pkg/polyflow/utils.go
+++ pkg/polyflow/utils.go
@@ -6,6 +6,51 @@ import (
 	"github.com/mouuff/polydictor/pkg/polyapi"
 )
 
+func GetPredictionRate(u *polyapi.User) float64 {
+	var totalPositions float64
+	var profitablePositions float64
+
+	for _, p := range u.ClosedPositions {
+		if p.RealizedPnl > 0 {
+			profitablePositions += 1
+		}
+
+		totalPositions += 1
+	}
+
+	if totalPositions == 0 {
+		return 0
+	}
+
+	return profitablePositions / totalPositions
+}
+
+func GetProfitRate(u *polyapi.User) float64 {
+	var netProfit float64
+	var totalInvestment float64
+
+	for _, p := range u.ClosedPositions {
+		netProfit += p.RealizedPnl
+		totalInvestment += p.TotalBought * p.AvgPrice
+	}
+
+	if totalInvestment == 0 {
+		return 0
+	}
+
+	return netProfit / totalInvestment
+}
+
+func GetProfit(u *polyapi.User) float64 {
+	var netProfit float64
+
+	for _, p := range u.ClosedPositions {
+		netProfit += p.RealizedPnl
+	}
+
+	return netProfit
+}
+
 func GetOutcomeIndexForOutcome(market *polyapi.Market, outcome string) (int, error) {
 	for i, o := range market.Outcomes {
 		if o == outcome {
