98647327a358b207428f19db813d8325feea90f0
Arnaud Aliès <arnaudalies.py@gmail.com>
2026-05-19 17:27:37 +0200

    Update analyzer.go

--- pkg/polyflow/analyzer.go
+++ pkg/polyflow/analyzer.go
@@ -24,7 +24,7 @@ func NewAnalyzer(db Store) *Analyzer {
 		ctx:               context.Background(),
 		api:               polyapi.NewPolyapi(),
 		db:                db,
-		UserCacheDuration: 6 * time.Hour,
+		UserCacheDuration: 12 * time.Hour,
 		UserMinimumBets:   10,
 		Debug:             true,
 	}
